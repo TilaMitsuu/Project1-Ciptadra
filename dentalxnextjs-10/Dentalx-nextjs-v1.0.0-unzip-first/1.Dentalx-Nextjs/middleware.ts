51925de44addfbdb821458fe290eb98121c052ad
TilaMitsuu <cantikatila610@gmail.com>
2025-05-28 16:21:12 +0700

    projek 1

---
+++ dentalxnextjs-10/Dentalx-nextjs-v1.0.0-unzip-first/1.Dentalx-Nextjs/middleware.ts
@@ -0,0 +1,7 @@
+// middleware.ts atau middleware.js
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+    return NextResponse.next()
+}
